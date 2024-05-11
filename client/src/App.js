4a539a262f117775f1fba5c60af890e2637b52d0
amanak7 <22je0094@iitism.ac.in>
2024-05-11 13:04:35 +0530

    home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, useNavigate, Form, useLocation} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Layouts/Navbar.jsx';
 import Login from './components/Auth/Login';
+// import Register from './components/Auth/Register';
+import Footer from './components/Layouts/Footer.jsx';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 import { useContext, useEffect } from 'react';
@@ -8,14 +11,16 @@ import { Context } from '.';
 import toast,{Toaster} from 'react-hot-toast';
 import axios from 'axios';
 import { BASE_URL } from './Base_url.js';
+
 function App() {
 
   const {isAuthorized,setIsAuthorized,user,setUser}=useContext(Context);
 
-
-  const location = window.location;
   const getUser = async () => {
     try {
+      if(location.pathname==='/medibuddy'){
+        return;
+      }
       const response = await axios.get(`${BASE_URL}/getuser`,{withCredentials:true});
       console.log("app response",response);
       setUser(response.data.user);
@@ -43,7 +48,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login />} />
-       <Route path='*' element={<NotFound />} />
+   <Route path='*' element={<NotFound />} />
     </Routes>
     <Footer />
     <Toaster />
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slider from './slider'
+// import Howitworks from './howitworks'
+import Features from './features'
+
+function Home() {
+  return (
+   <div className="">
+    <Slider />
+    <Features />
+   </div>
+  )
+}
+
+export default Home
