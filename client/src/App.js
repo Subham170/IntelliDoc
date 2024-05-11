951f5deed1e01894c16f56ede22dbd39db974b99
amanak7 <22je0094@iitism.ac.in>
2024-05-11 07:40:43 +0530

    login/signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route, useNavigate, Form, useLocation} from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import { useContext, useEffect } from 'react';
+import { Context } from '.';
+import toast,{Toaster} from 'react-hot-toast';
+import axios from 'axios';
+import { BASE_URL } from './Base_url.js';
 function App() {
+
+  const {isAuthorized,setIsAuthorized,user,setUser}=useContext(Context);
+
+
+  const location = window.location;
+  const getUser = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/getuser`,{withCredentials:true});
+      console.log("app response",response);
+      setUser(response.data.user);
+      setIsAuthorized(true);
+      toast.success(response.data.message);
+     
+    } catch (error) {
+      toast.success(error.response.data.message);
+      setIsAuthorized(false);
+      console.error('Error fetching user data:', error);
+
+    }
+  };
+  
+  useEffect(() => {
+    getUser();
+  }, [isAuthorized]);
+
+  console.log("auth->",isAuthorized);
+  console.log("user->",user);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Router>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+       <Route path='*' element={<NotFound />} />
+    </Routes>
+    <Footer />
+    <Toaster />
+   </Router>
+   </>
   );
 }
 
